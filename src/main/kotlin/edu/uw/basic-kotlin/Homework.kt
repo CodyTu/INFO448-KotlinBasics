1739bed1b39628c42276e90660d2f1777a4b9d87
CodyTu <ctu4@uw.edu>
2024-01-16 09:50:26 -0800

    completed basic kotlin assignment :

--- src/main/kotlin/edu/uw/basic-kotlin/Homework.kt
+++ src/main/kotlin/edu/uw/basic-kotlin/Homework.kt
@@ -4,6 +4,7 @@ package edu.uw.basickotlin
 fun whenFn(arg: Any): String {
     when (arg) {
         "Hello" -> return("world")
+        is String -> return("Say what?")
         0 -> return("zero")
         1 -> return("one")
         in 2..10 -> return("low number")
@@ -13,23 +14,46 @@ fun whenFn(arg: Any): String {
 }
 
 // write an "add" function that takes two Ints, returns an Int, and adds the values
-fun add(lhs: Int, rhs: Int): Int {
-    return lhs + rhs
-}
+fun add(lhs: Int, rhs: Int): Int = lhs + rhs
 
 // write a "sub" function that takes two Ints, returns an Int, and subtracts the values
-fun sub(lhs: Int, rhs: Int): Int {
-    return lhs - rhs
-}
+fun sub(lhs: Int, rhs: Int): Int = lhs - rhs
 
 // write a "mathOp" function that takes two Ints and a function (that takes two Ints and returns an Int), returns an Int, and applies the passed-in-function to the arguments
-fun mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int): Int {
-    return op(lhs, rhs)
-}
+fun mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int): Int = op(lhs, rhs)
 
 // write a class "Person" with first name, last name and age
-class Person(firstName: String, lastName: String, age: Int) {
-    constructor()
+class Person(val firstName: String, val lastName: String, val age: Int) {
+    val debugString: String = "[Person firstName:${firstName} lastName:${lastName} age:${age}]"
 }
 
 // write a class "Money"
+class Money(val amount: Int, val currency: String) {
+    val acceptedCurrencies = listOf("USD", "EUR", "CAN", "GBP")
+    init {
+        require(amount > 0 && currency in acceptedCurrencies)
+    }
+        
+    val toString: String = "[${amount}, ${currency}]"
+
+    public fun convert(conversionCurrency: String): Money {
+            
+        fun convertUSD(amount: Int, currency: String): Int {
+            when (currency) {
+                "EUR" -> return (2 * amount) / 3
+                "CAN" -> return (4 * amount) / 5
+                "GBP" -> return 2 * amount
+                else -> return amount
+            }
+        }
+            
+        when (conversionCurrency) {
+            "EUR" -> return Money((convertUSD(this.amount, this.currency)*3)/2, conversionCurrency)
+            "GBP" -> return Money(convertUSD(this.amount, this.currency)/2, conversionCurrency)
+            "CAN" -> return Money((convertUSD(this.amount, this.currency)*5)/4, conversionCurrency)
+            else -> return Money(convertUSD(this.amount, this.currency), conversionCurrency)
+        }
+    }
+
+    operator fun plus(other: Money): Money = Money(this.amount + other.convert(this.currency).amount, this.currency)
+}
